194bb985a2f6356e348350051774c2af0582ac38
Ale Pereira <naitwa-alexandre@gmail.com>
2025-09-12 16:40:06 -0300

    classes class 02

--- Class-2/Class-2/Main.cpp
+++ Class-2/Class-2/Main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
+struct Cordinates
 {
-	cout << "Hello World" << endl;
+	int x, y;
 
-	cin.get();
+	void Print()
+	{
+		std::cout << x << ", " << y << std::endl;
+	}
+};
+
+int main2()
+{
+	Cordinates cor;
+	cor.x = 2;
+	cor.y = 8;
+
+	Cordinates cor2;
+
+	cor2.x = 5;
+	cor2.y = 9;
+
+	cor.Print();
+	cor2.Print();
 
 	return 0;
 }
