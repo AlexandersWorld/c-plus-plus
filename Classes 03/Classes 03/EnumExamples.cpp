9be0d281a2af90268c5dcfda36648f7a9ee9e567
Ale Pereira <naitwa-alexandre@gmail.com>
2025-09-14 10:13:49 -0300

    finished destructors class

--- Classes 03/Classes 03/EnumExamples.cpp
+++ Classes 03/Classes 03/EnumExamples.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-
-
-enum Example : char
-{
-	A = 5, B, C
-};
-
-int a = 0;
-int b = 1;
-int c = 2;
-
-int main1()
-{
-
-	Example value = B;
-
-	if (Example::B == B)
-	{
-		std::cout << "Hello World" << std::endl;
-	}
-
-
-	std::cin.get();
-	return 0;
-}
+//#include <iostream>
+//
+//enum Example : char
+//{
+//	A = 5, B, C
+//};
+//
+//int a = 0;
+//int b = 1;
+//int c = 2;
+//
+//int main1()
+//{
+//
+//	Example value = B;
+//
+//	if (Example::B == B)
+//	{
+//		std::cout << "Hello World" << std::endl;
+//	}
+//
+//
+//	std::cin.get();
+//	return 0;
+//}
