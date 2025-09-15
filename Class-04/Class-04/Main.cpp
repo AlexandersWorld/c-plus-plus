3a3757e47da100957a9bf21aca8a3c613bf9fbd5
Ale Pereira <naitwa-alexandre@gmail.com>
2025-09-15 19:37:57 -0300

    virtual functions

---
+++ Class-04/Class-04/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Entity {
+public:
+	std::string GetName() { return "Entity"; }
+};
+
+class Player : public Entity {
+private:
+	std::string m_Name;
+public:
+	Player(const std::string& name) : m_Name(name) {}
+
+	std::string GetName() { return m_Name; }
+};
+
+void PrintName(Entity* entity)
+{
+	std::cout << entity->GetName() << std::endl;
+}
+
+int main()
+{
+	Entity* e = new Entity();
+	PrintName(e);
+
+	Player* p = new Player("Zagalo13");
+	PrintName(p);
+
+	return 0;
+}
