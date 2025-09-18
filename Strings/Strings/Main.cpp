3a5cd0aa714631954187098de9fa54c5ce9a04a2
Ale Pereira <naitwa-alexandre@gmail.com>
2025-09-18 11:41:32 -0300

    strings const

--- Strings/Strings/Main.cpp
+++ Strings/Strings/Main.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <string>
+
+class Entity
+{
+private:
+	int m_X, m_Y;
+public:
+	int GetX() const
+	{
+		return m_X;
+	}
+	void SetX(int x)
+	{
+		m_X = x;
+	}
+};
+
+static void Print(const Entity& e)
+{
+	std::cout << e.GetX() << std::endl;
+}
 
 int main()
 {
@@ -9,6 +30,8 @@ int main()
 	std::cout << name << std::endl;
 	std::cout << hw << std::endl;
 
+	Entity e;
+	const int es = e.GetX();
 
 	std::cin.get();
 	return 0;
