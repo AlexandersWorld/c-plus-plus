e55ba9d75b3d720f48b0719ce5032afebc63983c
Ale Pereira <naitwa-alexandre@gmail.com>
2025-09-22 08:55:47 -0300

    new lesson

---
+++ Operator-Class/Operator-Class/Operator-Class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Entity
+{
+private:
+    std::string m_Name;
+
+public:
+    Entity(): m_Name("Unkown") {}
+    Entity(const std::string name) : m_Name(name) {}
+
+    const std::string& GetName() const { return m_Name; }
+};
+
+int main()
+{
+    int a = 2;
+    int* b = new int[50]; // 200 byts;
+
+    Entity* e = new Entity();
+
+    delete e;
+    delete[] b;
+
+    std::cout << "Hello World!\n";
+}
