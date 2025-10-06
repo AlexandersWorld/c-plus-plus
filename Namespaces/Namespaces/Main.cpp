be07119f229e00b4392f1a8db614741fe05ab75b
Ale Pereira <naitwa-alexandre@gmail.com>
2025-10-06 11:49:47 -0300

    namespace class

---
+++ Namespaces/Namespaces/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+namespace apple {
+	void print(const std::string& text)
+	{
+		std::cout << text << std::endl;
+	}
+}
+
+namespace orange
+{
+	void print(const char* text)
+	{
+		std::string temp = text;
+		std::reverse(temp.begin(),temp.end());
+		std::cout << temp << std::endl;
+	}
+}
+
+int main()
+{
+	orange::print("Hello, World!");
+	apple::print("Hello, World!");
+
+	std::cin.get();
+	return 0;
+}
