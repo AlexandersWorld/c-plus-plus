cdff4dad1b24011770f94a900bcac74878bbdb6a
Ale Pereira <naitwa-alexandre@gmail.com>
2025-10-10 09:23:10 -0300

    starting lesson

---
+++ Type&Unions/Type&Unions/Pratice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+template<typename T>
+class Entity
+{
+private:
+	std::vector<T> entities;
+
+public:
+	Entity(std::vector<T>& array) : entities(array) {}
+
+	void PrintSize() const {
+		std::cout << "Size: " << entities.size() << std::endl;
+	}
+};
+
+int main1()
+{
+	std::vector<int> e = { 1,2,3 };
+
+	Entity<int>* entity = new Entity<int>(e);
+
+	entity->PrintSize();
+
+	delete entity;
+
+	std::cin.get();
+	return 0;
+}
