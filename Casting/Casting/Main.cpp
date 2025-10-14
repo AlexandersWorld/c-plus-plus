967217b7c35a2b64a9c60b0fa708731a4dcb3a52
Ale Pereira <naitwa-alexandre@gmail.com>
2025-10-14 09:24:07 -0300

    virtual destructor lesson

---
+++ Casting/Casting/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Base
+{
+public:
+	Base() { std::cout << "Base Constructor\n"; }
+	virtual ~Base() { std::cout << "Base Destroctor\n"; }
+};
+
+class Derived : public Base
+{
+public:
+	Derived() { std::cout << "Derived Constructor\n"; }
+	~Derived() { std::cout << "Derived Destroctor\n"; }
+};
+
+int main()
+{
+	std::cout << "Alexander's world!" << std::endl;
+
+	Base* base = new Base();
+	delete base;
+	std::cout << "-----------------------------\n";
+	Derived* derived = new Derived();
+	delete derived;
+	std::cout << "-----------------------------\n";
+	Base* poly = new Derived();
+	delete poly;
+
+
+	std::cin.get();
+	return 0;
+}
